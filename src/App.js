cd969192a62871ae22cf4b5263ce10345817441d
shanky-ced <shashankrai2022jun@cedcoss.com>
2022-08-22 19:22:18 +0530

    Feature added , backgroundChange yet to be added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import WeatherComponent from './Components/WeatherComponent';
 
+var flag = true;
+var data = [];
 function App() {
-  return (
-    <>
-    </>
-  );
-}
+  var [location, setLocation] = useState('');
+  var [Weather, setWeather] = useState([]);
+  var [count, setCount] = useState(false);
 
+  const locate = (e) => {
+    if (e.key === 'Enter') {
+      fetch(`https://api.weatherapi.com/v1/current.json?key=0bab7dd1bacc418689b143833220304&q=${location}`)
+        .then(resp => resp.json())
+        .then(data => {
+          setWeather(data);
+          setCount();
+        }).catch(err => {
+          document.querySelector('#warning').innerHTML = err;
+        });
+    }
+  }
+
+  useEffect(() => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=0bab7dd1bacc418689b143833220304&q=lucknow`)
+      .then(resp => resp.json())
+      .then(data => {
+        setCount(count + 1);
+        setWeather(data);
+      });
+    }, []);
+
+
+  return (
+    <div id='container'>
+      <div id='detailsContainer'>
+        <h1> Welcome to Weather Finder</h1>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}> <input type="text" onKeyPress={locate} placeholder="Search here" autoFocus id='input' onChange={e => setLocation(e.target.value)} /> <i className="fa-solid fa-magnifying-glass" ></i></div>
+      </div>
+      { Weather.length !== 0 ?  <WeatherComponent Weather={Weather} /> : ""}
+      <div id='warning'></div>
+    </div>
+  )
+}
 export default App;
+
---
+++ src/Components/WeatherComponent.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const WeatherComponent = ({ Weather }) => {
+
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+    
+        return `${day} ${date} ${month} ${year}`
+      }
+
+    return (
+        <>
+            <div id="weatherContainer">
+                <div> <h1 style={{fontSize:"2.5vw"}}> {Weather.location.name}, {Weather.location.country} </h1>
+                <span> <img src={`https:${Weather.current.condition.icon}`} alt="weather" style={{width:"15vh"}} /></span></div>
+                <div><span style={{fontSize:"2vw"}}>{dateBuilder(new Date(Weather.location.localtime))}</span></div>
+                <div>
+                    <div>
+                        <i className="fa-solid fa-wind"></i>
+                        <span>{Weather.current.wind_kph} kph (<p style={{fontSize:"3vh",display:"inline"}}>Direction : {Weather.current.wind_dir} </p>) </span>
+                    </div>
+                    <div>
+                        <i className="fa-solid fa-cloud"></i>
+                        <span>{Weather.current.cloud}</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <i className="fa-solid fa-temperature-high"></i>
+                        <span>{Weather.current.temp_c}<sup>o</sup> C </span>
+                        <span style={{ fontSize: "2.5vh" }}>Feels like {Weather.current.feelslike_c}<sup>o</sup> C </span>
+                    </div>
+                    <div>
+                        {Weather.current.is_day === 0 ?  <> <i className="fa-solid fa-moon"></i><span>Night</span> </> : <><i className="fa-solid fa-sun"></i> <span>Day</span></>}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WeatherComponent
